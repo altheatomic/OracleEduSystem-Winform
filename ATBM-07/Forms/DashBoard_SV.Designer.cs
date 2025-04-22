bd9be44037a86f803216f286e886927de27923d4
KhaiSV <22127174@student.hcmus.edu.vn>
2025-04-22 12:23:09 +0700

    SV design

--- ATBM-07/Forms/DashBoard_SV.Designer.cs
+++ ATBM-07/Forms/DashBoard_SV.Designer.cs
@@ -28,12 +28,388 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SV";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            textBox7 = new TextBox();
+            label7 = new Label();
+            textBox8 = new TextBox();
+            label8 = new Label();
+            textBox9 = new TextBox();
+            label9 = new Label();
+            label6 = new Label();
+            groupBox1 = new GroupBox();
+            btnUsers = new Button();
+            label10 = new Label();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            textBox6 = new TextBox();
+            label11 = new Label();
+            button2 = new Button();
+            groupBox2 = new GroupBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.ForeColor = Color.Teal;
+            label1.Location = new Point(63, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 21);
+            label1.TabIndex = 1;
+            label1.Text = "MASV";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F);
+            textBox1.Location = new Point(127, 55);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(120, 29);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(127, 99);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(120, 29);
+            textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.ForeColor = Color.Teal;
+            label2.Location = new Point(76, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 21);
+            label2.TabIndex = 3;
+            label2.Text = "Phái";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F);
+            textBox3.Location = new Point(127, 148);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(467, 29);
+            textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.ForeColor = Color.Teal;
+            label3.Location = new Point(59, 151);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Địa chỉ";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F);
+            textBox4.Location = new Point(474, 55);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(120, 29);
+            textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.ForeColor = Color.Teal;
+            label4.Location = new Point(406, 58);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 21);
+            label4.TabIndex = 7;
+            label4.Text = "Họ tên";
+            label4.TextAlign = ContentAlignment.TopCenter;
+            label4.Click += label4_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 12F);
+            textBox5.Location = new Point(474, 99);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(120, 29);
+            textBox5.TabIndex = 10;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F);
+            label5.ForeColor = Color.Teal;
+            label5.Location = new Point(382, 102);
+            label5.Name = "label5";
+            label5.Size = new Size(80, 21);
+            label5.TabIndex = 9;
+            label5.Text = "Ngày sinh";
+            label5.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 12F);
+            textBox7.Location = new Point(474, 203);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(120, 29);
+            textBox7.TabIndex = 16;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F);
+            label7.ForeColor = Color.Teal;
+            label7.Location = new Point(417, 206);
+            label7.Name = "label7";
+            label7.Size = new Size(45, 21);
+            label7.TabIndex = 15;
+            label7.Text = "Khoa";
+            label7.TextAlign = ContentAlignment.TopCenter;
+            label7.Click += label7_Click;
+            // 
+            // textBox8
+            // 
+            textBox8.Font = new Font("Segoe UI", 12F);
+            textBox8.Location = new Point(127, 247);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(120, 29);
+            textBox8.TabIndex = 14;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F);
+            label8.ForeColor = Color.Teal;
+            label8.Location = new Point(35, 250);
+            label8.Name = "label8";
+            label8.Size = new Size(81, 21);
+            label8.TabIndex = 13;
+            label8.Text = "Tình trạng";
+            label8.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox9
+            // 
+            textBox9.Font = new Font("Segoe UI", 12F);
+            textBox9.Location = new Point(127, 203);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(120, 29);
+            textBox9.TabIndex = 12;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F);
+            label9.ForeColor = Color.Teal;
+            label9.Location = new Point(35, 206);
+            label9.Name = "label9";
+            label9.Size = new Size(81, 21);
+            label9.TabIndex = 11;
+            label9.Text = "Điện thoại";
+            label9.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F);
+            label6.ForeColor = Color.Teal;
+            label6.Location = new Point(282, 29);
+            label6.Name = "label6";
+            label6.Size = new Size(170, 21);
+            label6.TabIndex = 17;
+            label6.Text = "THÔNG TIN SINH VIÊN";
+            label6.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnUsers);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox9);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Location = new Point(39, 73);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(643, 355);
+            groupBox1.TabIndex = 18;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // btnUsers
+            // 
+            btnUsers.AutoSize = true;
+            btnUsers.FlatAppearance.BorderColor = Color.Teal;
+            btnUsers.FlatAppearance.MouseDownBackColor = Color.DarkSlateGray;
+            btnUsers.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 255, 128);
+            btnUsers.FlatStyle = FlatStyle.Flat;
+            btnUsers.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnUsers.ForeColor = Color.Teal;
+            btnUsers.Location = new Point(254, 305);
+            btnUsers.Name = "btnUsers";
+            btnUsers.Size = new Size(150, 33);
+            btnUsers.TabIndex = 92;
+            btnUsers.Text = "Cập nhật";
+            btnUsers.UseVisualStyleBackColor = true;
+            btnUsers.Click += btnUsers_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F);
+            label10.ForeColor = Color.Teal;
+            label10.Location = new Point(293, 451);
+            label10.Name = "label10";
+            label10.Size = new Size(159, 21);
+            label10.TabIndex = 19;
+            label10.Text = "ĐĂNG KÝ HỌC PHẦN";
+            label10.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(35, 83);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(240, 150);
+            dataGridView1.TabIndex = 20;
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.FlatAppearance.BorderColor = Color.Teal;
+            button1.FlatAppearance.MouseDownBackColor = Color.DarkSlateGray;
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 255, 128);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Teal;
+            button1.Location = new Point(448, 617);
+            button1.Name = "button1";
+            button1.Size = new Size(64, 33);
+            button1.TabIndex = 93;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_3;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 12F);
+            textBox6.Location = new Point(522, 564);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(120, 29);
+            textBox6.TabIndex = 94;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 12F);
+            label11.ForeColor = Color.Teal;
+            label11.Location = new Point(448, 567);
+            label11.Name = "label11";
+            label11.Size = new Size(62, 21);
+            label11.TabIndex = 93;
+            label11.Text = "MAMM";
+            label11.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button2
+            // 
+            button2.AutoSize = true;
+            button2.FlatAppearance.BorderColor = Color.Teal;
+            button2.FlatAppearance.MouseDownBackColor = Color.DarkSlateGray;
+            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 255, 128);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.Teal;
+            button2.Location = new Point(448, 727);
+            button2.Name = "button2";
+            button2.Size = new Size(64, 33);
+            button2.TabIndex = 95;
+            button2.Text = "Xóa";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Location = new Point(39, 493);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(643, 366);
+            groupBox2.TabIndex = 96;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // DashBoard_SV
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1894, 1009);
+            Controls.Add(button2);
+            Controls.Add(textBox6);
+            Controls.Add(label11);
+            Controls.Add(button1);
+            Controls.Add(label10);
+            Controls.Add(groupBox1);
+            Controls.Add(label6);
+            Controls.Add(groupBox2);
+            Name = "DashBoard_SV";
+            Text = "SV";
+            Load += DashBoard_SV_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+        private TextBox textBox7;
+        private Label label7;
+        private TextBox textBox8;
+        private Label label8;
+        private TextBox textBox9;
+        private Label label9;
+        private Label label6;
+        private GroupBox groupBox1;
+        private Button btnUsers;
+        private Label label10;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private TextBox textBox6;
+        private Label label11;
+        private Button button2;
+        private GroupBox groupBox2;
     }
 }
--- ATBM-07/Forms/DashBoard_SV.cs
+++ ATBM-07/Forms/DashBoard_SV.cs
@@ -16,5 +16,65 @@ namespace ATBM_07.Forms
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DashBoard_SV_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUsers_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
